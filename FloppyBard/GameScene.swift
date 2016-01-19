4a5ff9b25722ae38d0b44844b0c7c07753a2e325
Julian Nicholls <juliannicholls29@gmail.com>
2016-01-19 11:21:17 +0000

    Add collision detection
    Ground level appears to be at y=96

--- FloppyBard/GameScene.swift
+++ FloppyBard/GameScene.swift
@@ -16,6 +16,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var pipe1 = SKSpriteNode()
     var pipe2 = SKSpriteNode()
 
+    enum ColliderType : UInt32 {
+        case Bird   = 1
+        case Object = 2
+    }
+
+    var gameOver = false
+
     override func didMoveToView(view: SKView) {
         self.physicsWorld.contactDelegate = self
         self.physicsWorld.gravity = CGVectorMake(0, -6.0)
@@ -28,12 +35,24 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        bird.physicsBody!.velocity = CGVectorMake(0, 0)
-        bird.physicsBody?.applyImpulse(CGVectorMake(0, 40))
+        if gameOver {
+
+        }
+        else {
+            bird.physicsBody!.velocity = CGVectorMake(0, 0)
+            bird.physicsBody?.applyImpulse(CGVectorMake(0, 40))
+        }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+
+    func didBeginContact(contact: SKPhysicsContact) {
+        print("Contact")
+
+        gameOver = true
+        self.speed = 0
     }
 
     func makeBackground() {
@@ -59,10 +78,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func makeGround() {
         let ground = SKNode()
 
-        ground.position = CGPointMake(0, 90)
+        ground.position = CGPointMake(0, 96)
 
         ground.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(self.frame.size.width, 1))
         ground.physicsBody?.dynamic = false
+
+        bird.physicsBody?.categoryBitMask       = ColliderType.Object.rawValue
+        bird.physicsBody?.contactTestBitMask    = ColliderType.Object.rawValue
+        bird.physicsBody?.collisionBitMask      = ColliderType.Object.rawValue
 
         self.addChild(ground)
     }
@@ -82,6 +105,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         bird.physicsBody = SKPhysicsBody(circleOfRadius: bird.size.height / 2)
         bird.physicsBody?.dynamic = true
 
+        bird.physicsBody?.categoryBitMask       = ColliderType.Bird.rawValue
+        bird.physicsBody?.contactTestBitMask    = ColliderType.Object.rawValue
+        bird.physicsBody?.collisionBitMask      = ColliderType.Object.rawValue
+
         self.addChild(bird)
     }
 
@@ -96,16 +123,36 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let removePipes     = SKAction.removeFromParent()
         let moveandRemove   = SKAction.sequence([movePipes, removePipes])
 
+        // Pipe 1
+
         pipe1 = SKSpriteNode(texture: pipe1Texture)
-        pipe2 = SKSpriteNode(texture: pipe2Texture)
 
         pipe1.position = CGPoint(x: CGRectGetMaxX(self.frame) + self.frame.size.width, y: CGRectGetMidY(self.frame) + pipe1Texture.size().height / 2 + gap / 2 + offset)
-        pipe2.position = CGPoint(x: CGRectGetMaxX(self.frame) + self.frame.size.width, y: CGRectGetMidY(self.frame) - pipe2Texture.size().height / 2 - gap / 2 + offset)
-
         pipe1.runAction(moveandRemove)
-        pipe2.runAction(moveandRemove)
+
+        pipe1.physicsBody = SKPhysicsBody(rectangleOfSize: pipe1Texture.size())
+        pipe1.physicsBody?.dynamic = false
+
+        pipe1.physicsBody?.categoryBitMask       = ColliderType.Object.rawValue
+        pipe1.physicsBody?.contactTestBitMask    = ColliderType.Object.rawValue
+        pipe1.physicsBody?.collisionBitMask      = ColliderType.Object.rawValue
 
         self.addChild(pipe1)
+
+        // Pipe 2
+
+        pipe2 = SKSpriteNode(texture: pipe2Texture)
+
+        pipe2.position = CGPoint(x: CGRectGetMaxX(self.frame) + self.frame.size.width, y: CGRectGetMidY(self.frame) - pipe2Texture.size().height / 2 - gap / 2 + offset)
+        pipe2.runAction(moveandRemove)
+
+        pipe2.physicsBody = SKPhysicsBody(rectangleOfSize: pipe2Texture.size())
+        pipe2.physicsBody?.dynamic = false
+
+        pipe2.physicsBody?.categoryBitMask       = ColliderType.Object.rawValue
+        pipe2.physicsBody?.contactTestBitMask    = ColliderType.Object.rawValue
+        pipe2.physicsBody?.collisionBitMask      = ColliderType.Object.rawValue
+
         self.addChild(pipe2)
     }
 }
